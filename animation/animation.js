d7255ce7f10ec0e373b660e1bfbc424d86d86a58
Daviz <37458551+Davizf@users.noreply.github.com>
2018-10-11 14:58:41 +0200

    Add files via upload

--- animation/animation.js
+++ animation/animation.js
@@ -57,3 +57,57 @@ function opinionBox3(){
 function cerrarPopup(n){
   document.getElementById(n).style.display="none";
 }
+
+
+function logout(){
+  $("#menuBlock1").hide();
+  $("#leftContent").hide();
+  $("#rightContent").hide();
+  $("#hiddenMenu").show();
+
+}
+
+function register(){
+  $(".rightBox").hide();
+  $(".leftBox").hide();
+  $("#registerBox").show();
+}
+
+
+/*  W3school copy and paste
+Problem -> can save the cookie file in the path
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
+*/
